4a183441776b5680c48b816fad70d34954ed6961
Gang Zhang <gzhangx@hotmail.com>
2026-05-16 11:34:44 -0400

    fix axis

--- src/components/Viewport3D/AxesHelper.tsx
+++ src/components/Viewport3D/AxesHelper.tsx
@@ -1,20 +1,22 @@
 import { Line, Text } from '@react-three/drei'
+import { PLANE_SIZE } from '../../lib/units'
 
 export function AxesHelper() {
-  const len = 3
+  const len = PLANE_SIZE * 0.16
+  const labelOffset = 0.6
   return (
     <group>
       {/* X axis - red */}
       <Line points={[[0,0,0],[len,0,0]]} color="#ff4444" lineWidth={2} />
-      <Text position={[len + 0.2, 0, 0]} fontSize={0.25} color="#ff4444">X</Text>
+      <Text position={[len + labelOffset, 0, 0]} fontSize={0.32} color="#ff4444">X</Text>
 
       {/* Y axis - green */}
       <Line points={[[0,0,0],[0,len,0]]} color="#44ff44" lineWidth={2} />
-      <Text position={[0, len + 0.2, 0]} fontSize={0.25} color="#44ff44">Y</Text>
+      <Text position={[0, len + labelOffset, 0]} fontSize={0.32} color="#44ff44">Y</Text>
 
       {/* Z axis - blue */}
       <Line points={[[0,0,0],[0,0,len]]} color="#4488ff" lineWidth={2} />
-      <Text position={[0, 0, len + 0.2]} fontSize={0.25} color="#4488ff">Z</Text>
+      <Text position={[0, 0, len + labelOffset]} fontSize={0.32} color="#4488ff">Z</Text>
     </group>
   )
 }
